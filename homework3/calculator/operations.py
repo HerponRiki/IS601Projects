f4d04bf6860b0fb52a016887d80a61aa2535029f
HerponRiki <ashyang586@gmail.com>
2024-09-26 23:41:22 -0400

    Added part 3 and fixed up come parts of the code

--- homework3/calculator/operations.py
+++ homework3/calculator/operations.py
@@ -1,11 +1,15 @@
-def add(_a,_b):
+from decimal import Decimal
+# Define the functions with type hints
+def add(_a: Decimal, _b: Decimal) -> Decimal:
     return _a + _b
 
-def subtract(_a,_b):
+def subtract(_a: Decimal, _b: Decimal) -> Decimal:
     return _a - _b
 
-def multiply(_a,_b):
+def multiply(_a: Decimal, _b: Decimal) -> Decimal:
     return _a * _b
 
-def divide(_a,_b):
+def divide(_a: Decimal, _b: Decimal) -> Decimal:
+    if _b == 0:
+        raise ValueError("Cannot divide by zero")
     return _a / _b
