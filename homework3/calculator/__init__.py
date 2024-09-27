358a3c6697b8a0c91587eeffbfed844be7b4cba1
HerponRiki <ashyang586@gmail.com>
2024-09-26 22:31:54 -0400

    Basic calculator code

---
+++ homework3/calculator/__init__.py
@@ -0,0 +1,18 @@
+"""
+Calculator code
+"""
+def add(_a,_b):
+    """Adds"""
+    return _a + _b
+
+def subtract(_a,_b):
+    """Subtracts"""
+    return _a - _b
+
+def multiply(_a,_b):
+    """Multiplys"""
+    return _a * _b
+
+def divide(_a,_b):
+    """Divides"""
+    return _a/_b
